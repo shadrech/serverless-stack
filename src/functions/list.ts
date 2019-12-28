df0b5aea01bab2fa72157649d4e4c94747d0c4b7
Tatenda Chawanzwa <tatenda@chadigital.com>
2019-12-28 23:46:07 +0000

    bad handler fix attempt

--- src/functions/list.ts
+++ src/functions/list.ts
@@ -3,7 +3,7 @@ import { AWSLambdaHandler } from '../typings';
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-const get: AWSLambdaHandler = async (event) => {
+export const main: AWSLambdaHandler = async (event) => {
   const params = {
     TableName: process.env.TABLE_NAME || '',
     KeyConditionExpression: 'userId = :userId',
@@ -33,5 +33,3 @@ const get: AWSLambdaHandler = async (event) => {
     body: JSON.stringify(result.Items)
   };
 };
-
-export default get;
